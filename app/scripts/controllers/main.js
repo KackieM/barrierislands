6ee9ea97bc4d6489246e01e0c73a2ad16a1ab09f
Katherine Mattison <kbmedisto@gmail.com>
2014-07-31 20:45:09 -0400

    decided to go with angularjs for the build

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('barrierislandsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
